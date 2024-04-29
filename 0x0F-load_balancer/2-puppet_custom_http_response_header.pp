b7ecaf633a6c8a681e51be145d069b349f30615e
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-30 01:06:57 +0300

    Updating 2-puppet_custom_http_response_header.pp file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -11,13 +11,11 @@ provider        => 'apt',
 install_options => ['-y'],
 }
 
-$new_header="\tadd_header X-Served-By \$hostname;"
-
 file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen \[::\]:80 default_server;',
-  line   => $new_header,
+  line   => add_header X-Served-By $hostname;,
 }
 
 service { 'nginx':
