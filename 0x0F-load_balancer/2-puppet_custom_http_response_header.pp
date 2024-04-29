5f481dda5d608bc241f2a1bd2a4e717791c92e42
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-04-30 00:55:03 +0300

    Updating 2-puppet_custom_http_response_header.pp file

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,21 @@
 # Puppet script to install and configure an Nginx server
 
+exec { 'update':
+  command  => 'apt-get update',
+  path     => '/usr/bin/'
+}
+
 package { 'nginx':
 ensure          => installed,
 provider        => 'apt',
 install_options => ['-y'],
 }
 
-$new_header="add_header X-Served-By $hostname;"
-
 file_line { 'add_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen \[::\]:80 default_server;',
-  line   => $new_header,
+  line   => 'add_header X-Served-By $hostname;',
 }
 
 service { 'nginx':
