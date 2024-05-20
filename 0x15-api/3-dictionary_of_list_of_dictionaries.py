8ae49e2033b9763659173f5a152939d1700ba60c
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-05-21 02:04:39 +0300

    Updating 3-dictionary_of_list_of_dictionaries.py file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,23 +11,28 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    users_request = requests.get("{}".format(users_url))
+    users_request = requests.get("https://jsonplaceholder.typicode.com/users")
+    tasks_request = requests.get("https://jsonplaceholder.typicode.com/todos")
 
     users = users_request.json()
+    tasks = tasks_request.json()
 
     file_name = "todo_all_employees.json"
     with open(file_name, 'w') as file:
 
         employees_tasks = {}
         for user in users:
-            user_tasks_request = requests.get("{}/{}/todos/"
-                                              .format(users_url, user['id']))
-            tasks = user_tasks_request.json()
-            employees_tasks[user['id']] = []
-            for task in tasks:
-                employees_tasks[user['id']].append(
-                    {"username": user['username'], "task": task['title'],
-                     "completed": task['completed']}
-                )
+            user_name = user['username']
+            user_id = user['id']
+
+            user_tasks = [
+                {
+                    "username": user_name,
+                    "task": task['title'],
+                    "completed": task['completed']
+                }
+                for task in tasks if task['userId'] == user_id
+                ]
+            employees_tasks[user_id] = user_tasks
+
         json.dump(employees_tasks, file)
