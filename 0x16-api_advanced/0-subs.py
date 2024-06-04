330fb66bf3add50eefea7a000e6893d18536da98
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-06-04 13:20:16 +0300

    Updating 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,8 @@ def number_of_subscribers(subreddit):
                AppleWebKit/537.36 (KHTML, like Gecko)\
                Chrome/125.0.0.0 Safari/537.36"}
 
-    url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
