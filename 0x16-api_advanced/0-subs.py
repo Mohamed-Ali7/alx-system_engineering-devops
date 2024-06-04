3ac53a8a662cf504dbe67b32e47f38164e133a14
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-06-04 15:33:50 +0300

    Updating 0-subs.py file and adding access_token file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+
+"""This module contains number_of_subscribers functions"""
+
+import requests
+from requests.auth import HTTPBasicAuth
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
+    """
+    Returns the number of subscribers (not active users, total subscribers)
+    for a given subreddit.
+    """
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    file_name = "access_token"
+    access_token = ''
+    with open(file_name, 'r') as file:
+        access_token = "bearer {}".format(file.read().strip())
+
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+               AppleWebKit/537.36 (KHTML, like Gecko)\
+               Chrome/125.0.0.0 Safari/537.36",
+               "Authorization": access_token}
+
+    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
