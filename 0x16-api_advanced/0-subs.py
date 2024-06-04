4d5c7e20c4556801cd0bd0308014ff29ffeb0e87
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-06-04 16:22:44 +0300

    Updating 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,17 +11,11 @@ def number_of_subscribers(subreddit):
     for a given subreddit.
     """
 
-    file_name = "access_token"
-    access_token = ''
-    with open(file_name, 'r') as file:
-        access_token = "bearer {}".format(file.read().strip())
-
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/125.0.0.0 Safari/537.36",
-               "Authorization": access_token}
+               Chrome/125.0.0.0 Safari/537.36"}
 
-    url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
