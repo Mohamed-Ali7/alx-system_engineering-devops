200fdda0075434ea637cf8c3f29b95aa2e7629f2
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-06-04 16:07:44 +0300

    Updating 1-top_ten.py file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,17 +10,11 @@ def top_ten(subreddit):
     prints the titles of the first 10 hot posts listed for a given subreddit
     """
 
-    file_name = "access_token"
-    access_token = ''
-    with open(file_name, 'r') as file:
-        access_token = "bearer {}".format(file.read().strip())
-
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                AppleWebKit/537.36 (KHTML, like Gecko)\
-               Chrome/125.0.0.0 Safari/537.36",
-               "Authorization": access_token}
+               Chrome/125.0.0.0 Safari/537.36"}
 
-    url = "https://oauth.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False,
                             params={"limit": 10})
 
