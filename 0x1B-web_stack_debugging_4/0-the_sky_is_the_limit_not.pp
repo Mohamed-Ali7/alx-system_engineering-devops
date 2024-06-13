b1972deaf19d857ed22ba68cd24a22e85030aa55
Mohamed Ali <mohammed.ali17955@gmail.com>
2024-06-13 19:23:59 +0300

    Adding 0x1B-web_stack_debugging_4 directory and README.md and 0-the_sky_is_the_limit_not.pp files

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Increases the number of the opened files that Nginx server can handle.
+
+exec { 'increase':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/bin/:/bin/'
+} ->
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
